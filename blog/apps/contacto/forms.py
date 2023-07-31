37697852dcb130b30ee347b4c496c103f3643e65
Your Name01 <rocioduarte197@gmail.com>
2023-07-31 18:39:22 -0300

    arreglando contacto y chequeando que se guarden en el admin django

--- blog/apps/contacto/forms.py
+++ blog/apps/contacto/forms.py
@@ -5,5 +5,5 @@ from .models import Contacto
 class ContactoForm(forms.ModelForm):
     class Meta:
         model = Contacto
-        fields = ['nombre_apellido','email','asunto','fecha']
+        fields = ['nombre_apellido','email','asunto','mensaje']
     
